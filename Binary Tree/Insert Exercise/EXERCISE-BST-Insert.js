2e401db75686a577bf4e6dbfb8a54134e59d5941
Johnny <JohnnyLieu@yahoo.com>
2023-06-03 10:35:31 -0700

    i can't believe I figured this out on my own LOOOLL

--- Binary Tree/Insert Exercise/EXERCISE-BST-Insert.js
+++ Binary Tree/Insert Exercise/EXERCISE-BST-Insert.js
@@ -5,25 +5,40 @@ class Node {
         this.right = null;
     }
 }
- 
+
 class BST {
     constructor() {
         this.root = null;
     }
 
-	/// WRITE INSERT METHOD HERE ///
-	//                            //
-	//                            //
-	//                            //
-	//                            //
-	////////////////////////////////
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        let temp = this.root;
+        while (true) {
+            if (newNode.value === temp.value) {
+                return undefined;
+            }
 
+            if (newNode.value < temp.value) {
+                if (temp.left === null) {
+                    temp.left = newNode;
+                    return this;
+                }
+            } else if (newNode.value > temp.value) {
+                if (temp.right === null) {
+                    temp.right = newNode;
+                    return this;
+                }
+            }
+        }
+    }
 }
 
-
-
 function test() {
-
     let myBST = new BST();
 
     myBST.insert(2);
@@ -37,15 +52,12 @@ function test() {
                    1   3
     */
 
-
     console.log("Root:", myBST.root.value);
     console.log("\nRoot->Left:", myBST.root.left.value);
     console.log("\nRoot->Right:", myBST.root.right.value);
 }
 
-
 test();
-
 
 /*
     EXPECTED OUTPUT:
@@ -57,4 +69,3 @@ test();
     Root->Right: 3
 
 */
-      
